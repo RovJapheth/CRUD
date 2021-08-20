6654fddf4ed355eb39eaf1580d917c4d19ca0a29
Your Name <roracion7@gmail.com>
2021-08-20 22:36:24 +0800

    first commit

---
+++ src/routes/auth/endpoints.js
@@ -0,0 +1,65 @@
+'use strict';
+var Handlers = require('./handlers'), internals = {};
+
+internals.endpoints = [
+
+    {
+        method: ['GET'],
+        path: '/',
+        handler: Handlers.log_in,
+        config: {
+            auth: false
+        }
+    },
+
+    {
+        method: [ 'GET', 'POST' ],
+        path: '/{any*}',
+        handler: Handlers.error404,
+        config: {
+          description: '404',
+          tags: ['api'],
+        }
+      },
+
+    {
+        method: ['POST'],
+        path: '/authenticate',
+        handler: Handlers.authenticate,
+        config: {
+            auth: false
+        }
+    },
+
+    {
+        method: ['GET'],
+        path: '/sign-up',
+        handler: Handlers.sign_up,
+        config: {
+            auth: false
+        }
+    },
+
+    {
+        method: ['POST'],
+        path: '/save-user',
+        handler: Handlers.save_user,
+        config: {
+            auth: false
+        }
+    },
+
+
+    {
+        method: ['GET'],
+        path: '/sign-out',
+        handler: Handlers.sign_out,
+        config: {
+            auth: false
+        }
+    },
+
+
+];
+
+module.exports = internals;
